8cba850616d1f81ae8f01443989d74e4aa488690
Polk <polkandrew01@gmail.com>
2019-12-18 16:06:42 -0800

    Change tab title per page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+  ) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const title = this.getTitle(router.routerState, router.routerState.root).join(' | ');
+        this.titleService.setTitle(title);
+      }
+    });
+  }
+
+  getTitle(state, parent) {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      if (parent.snapshot.params.id) {
+        data.push(parent.snapshot.params.id);
+      }
+      data.push(parent.snapshot.data.title);
+    }
+
+    if (state && parent) {
+      data.push(... this.getTitle(state, state.firstChild(parent)));
+    }
+    return data;
+  }
+
 }
