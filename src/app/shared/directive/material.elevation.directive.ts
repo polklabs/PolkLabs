90454108c940bf20df3374dc415b86b55d32f466
Polk <polkandrew01@gmail.com>
2019-12-18 16:07:15 -0800

    Fixed linting error

--- src/app/shared/directive/material.elevation.directive.ts
+++ src/app/shared/directive/material.elevation.directive.ts
@@ -34,7 +34,7 @@ export class MaterialElevationDirective implements OnChanges {
 
   setElevation(amount: number) {
     // remove all elevation classes
-    const classesToRemove = Array.from((<HTMLElement>this.element.nativeElement).classList).filter(c => c.startsWith('mat-elevation-z'));
+    const classesToRemove = Array.from((this.element.nativeElement as HTMLElement).classList).filter(c => c.startsWith('mat-elevation-z'));
     classesToRemove.forEach((c) => {
       this.renderer.removeClass(this.element.nativeElement, c);
     });
