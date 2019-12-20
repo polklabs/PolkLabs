a73d18ed1d02454287770daeeeeee0742489ae20
Polk <polkandrew01@gmail.com>
2019-12-20 12:19:16 -0800

    Updates for seo audits and best practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { mergeMap, filter, map } from 'rxjs/operators';
+import { SEOService } from './core/service/seo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   constructor(
     private titleService: Title,
     private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private seoService: SEOService,
   ) {
-    router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        const title = this.getTitle(router.routerState, router.routerState.root).join(' | ');
-        this.titleService.setTitle(title);
-      }
+    // router.events.subscribe(event => {
+    //   if (event instanceof NavigationEnd) {
+    //     const title = this.getTitle(router.routerState, router.routerState.root).join(' | ');
+    //     this.titleService.setTitle(title);
+    //   }
+    // });
+  }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data)
+    )
+    .subscribe((event) => {
+      const id = this.getTitle(this.router.routerState, this.router.routerState.root);
+
+      this.seoService.updateTitle(event.title + (id.length > 0 ? (' | ' + id[0]) : ''));
+      this.seoService.updateDescription(event.title + ' ' + event.description);
     });
   }
 
@@ -27,7 +52,6 @@ export class AppComponent {
       if (parent.snapshot.params.id) {
         data.push(parent.snapshot.params.id);
       }
-      data.push(parent.snapshot.data.title);
     }
 
     if (state && parent) {
